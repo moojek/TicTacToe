ef65c49327ff9a63ec0eb7d114c6bdebaca16f05
moojek <42188458+moojek@users.noreply.github.com>
2023-02-06 19:05:26 +0100

    Add proper routing to game.js and fix stylesheets directories links.

--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function (req, res, next) {
-    res.render('game', { title: 'Game - Tic Tac Toe', nickname1: '123', nickname2: '321'})
+router.get('/:id', function (req, res, next) {
+    res.render('game', {title: 'Game - Tic Tac Toe', nickname1: '123', nickname2: '321'})
 })
 
 module.exports = router
