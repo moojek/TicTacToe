789d682e6178a0a9c7f29c6332acc9a6b030d165
Piotr Traczynski <piotr.traczynski1@gmail.com>
2023-02-06 22:26:00 +0100

    Fix game

--- game_logic/game-server.js
+++ game_logic/game-server.js
@@ -4,31 +4,28 @@ var gameLogic = require('./game-logic')
 const setupGameServer = function (httpServer) {
     var io = socket(httpServer)
 
-    Rooms = []
+    var Rooms = []
+    var board = []
 
     io.on('connection', function (socket) {
         socket.on('create', function (params) {
             socket.join(params[0]);
+            board[params[0]] = []
             let sign = 'X'
             if (!Rooms[params[0]])
                 sign = 'O'
             Rooms[params[0]] = 'X'
-            let board = []
             let characters = {X: 'O', O: 'X'}
 
             console.log("connected: " + socket.id);
 
             socket.on('click', function (id) {
-                console.log(sign + "  " + Rooms[params[0]] + " --- " + board[parseInt(id)]);
-                if (board[parseInt(id)] || Rooms[params[0]] != sign)
+                if (board[params[0]][parseInt(id)] || Rooms[params[0]] != sign)
                     return
-                board[parseInt(id)] = Rooms[params[0]]
-                console.log(board)
+                board[params[0]][parseInt(id)] = Rooms[params[0]]
                 io.to(params[0]).emit('click', [id, Rooms[params[0]]]);
-                if (gameLogic.isWinner(board)) {
-                    io.to(params[0]).emit('winner', Rooms[params[0]] == 'O' ?  1 : 0)
-                    io.to(params[0]).emit('reset')
-                    board = []
+                if (gameLogic.isWinner(board[params[0]])) {
+                    //render game with the same users and other score
                 }
 
                 Rooms[params[0]] = characters[Rooms[params[0]]]
