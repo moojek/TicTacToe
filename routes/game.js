af04ac17a738589ebfece07ab4ced6adb3394b90
Piotr Traczynski <piotr.traczynski1@gmail.com>
2023-02-07 03:39:38 +0100

    Fix some bugs

--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var router = express.Router()
+var router = express.Router()
 var createError = require('http-errors')
 
-router.get('/:id/:name', function (req, res, next) {
-    res.render('game', {title: 'Game - Tic Tac Toe'}) 
+
+router.get('/', function (req, res, next) {
+    if (!req.query.roomId) {
+        next(createError(400), 'No \'roomId\' parameter')
+    } else
+        res.redirect('/game/' + req.query.roomId)
 })
 
+router.get('/:id/:name/', function (req, res, next) {
+    console.log(req.params.id)
+    res.render('game', {title: 'Game - Tic Tac Toe'})
+})
+
+
+module.exports = router
 module.exports = router
