009c3f969256985abfc5d0bdfce0279fcb9ab97a
moojek <42188458+moojek@users.noreply.github.com>
2023-02-07 02:22:33 +0100

    Redesign rooms.ejs and rooms pages accordingly + some routing redesign.

--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var router = express.Router()
+var createError = require('http-errors')
 
+router.get('/', function (req, res, next) {
+    if (!req.query.roomId) {
+        next(createError(400), 'No \'roomId\' parameter')
+    } else
+        res.redirect('/game/' + req.query.roomId)
+})
 router.get('/:id', function (req, res, next) {
     console.log(req.params.id)
-    res.render('game', {title: 'Game - Tic Tac Toe', nickname1: 'Piotrek', nickname2: 'Grzesiu', IDroom: req.params.id}) 
+    res.render('game', {title: 'Game - Tic Tac Toe', nickname1: 'Piotrek', nickname2: 'Grzesiu', roomId: req.params.id})
 })
 
 module.exports = router
